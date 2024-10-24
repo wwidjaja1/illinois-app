7681ddfc83a733a1959425f0544ec85a7ae4771d
wwidjaja1 <willgr81@gmail.com>
2024-10-24 18:49:50 -0400

    Update ExploreBuildingDetailPanel.dart
    Edited error handling for pulling svg

--- lib/ui/explore/ExploreBuildingDetailPanel.dart
+++ lib/ui/explore/ExploreBuildingDetailPanel.dart
@@ -18,13 +18,11 @@ import 'package:illinois/service/Auth2.dart';
 import 'package:rokwire_plugin/service/network.dart';
 import 'dart:convert';
 
-class ExploreBuildingDetailPanel extends StatefulWidget with AnalyticsInfo {
+class ExploreBuildingDetailPanel extends StatefulWidget {
   final Building? building;
   final String? buildingNumber;
-  final ExploreSelectLocationBuilder? selectLocationBuilder;
-  final AnalyticsFeature? analyticsFeature; //This overrides AnalyticsInfo.analyticsFeature getter
 
-  ExploreBuildingDetailPanel({super.key, this.building, this.buildingNumber, this.analyticsFeature, this.selectLocationBuilder });
+  ExploreBuildingDetailPanel({Key? key, this.building, this.buildingNumber}) : super(key: key);
 
   @override
   State<StatefulWidget> createState() => _ExploreBuildingDetailPanelState();
@@ -56,29 +54,29 @@ class _ExploreBuildingDetailPanelState extends State<ExploreBuildingDetailPanel>
 
   @override
   Widget build(BuildContext context) =>
-    Scaffold(
-      body: _buildScaffoldContent(),
-      backgroundColor: Styles().colors.background,
-      bottomNavigationBar: uiuc.TabBar()
-    );
+      Scaffold(
+          body: _buildScaffoldContent(),
+          backgroundColor: Styles().colors.background,
+          bottomNavigationBar: uiuc.TabBar()
+      );
 
   Widget _buildScaffoldContent() => Column(children: <Widget>[
     Expanded(child:
-      CustomScrollView(slivers: <Widget>[
-        SliverToutHeaderBar(
-          flexImageUrl: (_building != null) ? _building?.imageURL : null,
-          flexWidget: (_building == null) ? Container(color: Styles().colors.background,) : null,
-          flexRightToLeftTriangleColor: Styles().colors.background,
-          flexLeftToRightTriangleColor: Styles().colors.fillColorSecondaryTransparent05,
+    CustomScrollView(slivers: <Widget>[
+      SliverToutHeaderBar(
+        flexImageUrl: (_building != null) ? _building?.imageURL : null,
+        flexWidget: (_building == null) ? Container(color: Styles().colors.background,) : null,
+        flexRightToLeftTriangleColor: Styles().colors.background,
+        flexLeftToRightTriangleColor: Styles().colors.fillColorSecondaryTransparent05,
+      ),
+      SliverList(delegate:
+      SliverChildListDelegate([
+        Padding(padding: EdgeInsets.all(16), child:
+        _buildPanelContent()
         ),
-        SliverList(delegate:
-          SliverChildListDelegate([
-            Padding(padding: EdgeInsets.all(16), child:
-              _buildPanelContent()
-            ),
-          ], addSemanticIndexes:false)
-        ),
-      ]),
+      ], addSemanticIndexes:false)
+      ),
+    ]),
     ),
   ]);
 
@@ -95,98 +93,91 @@ class _ExploreBuildingDetailPanelState extends State<ExploreBuildingDetailPanel>
   }
 
   Widget _buildBuildingContent() =>
-    Column(mainAxisSize: MainAxisSize.min, crossAxisAlignment: CrossAxisAlignment.start, children: [
-      _buildTitle(),
-      _buildLocation(),
-      _buildShare(),
-      // _buildFloorPlansAndAmenities(),
-      _buildSelectLocation(),
-    ]);
+      Column(mainAxisSize: MainAxisSize.min, crossAxisAlignment: CrossAxisAlignment.start, children: [
+        _buildTitle(),
+        _buildLocation(),
+        _buildShare(),
+        _buildFloorPlansAndAmenities(),
+      ]);
 
   Widget _buildTitle() =>
-    Padding(padding: EdgeInsets.symmetric(vertical: 10), child:
+      Padding(padding: EdgeInsets.symmetric(vertical: 10), child:
       Row(crossAxisAlignment: CrossAxisAlignment.center, mainAxisAlignment: MainAxisAlignment.start, children: <Widget>[
         Expanded(child:
-          Text(_building?.name ?? "", style: Styles().textStyles.getTextStyle("widget.title.large.fat")),
+        Text(_building?.name ?? "", style: Styles().textStyles.getTextStyle("widget.title.large.fat")),
         ),
       ],),
-    );
+      );
 
   Widget _buildLocation() =>
-    Visibility(visible: _canLocation(), child:
+      Visibility(visible: _canLocation(), child:
       InkWell(onTap: _onLocation, child:
-        Padding(padding: EdgeInsets.symmetric(vertical: 10, ), child:
-          Row(children: [
-            Padding(padding: EdgeInsets.only(right: 6), child:
-              Styles().images.getImage('location', excludeFromSemantics: true),
-            ),
-            Expanded(child:
-              Text(_building?.fullAddress ?? '', style:
-                Styles().textStyles.getTextStyle("widget.button.light.title.medium.underline")
-              ),
-            )
-          ],),
+      Padding(padding: EdgeInsets.symmetric(vertical: 10, ), child:
+      Row(children: [
+        Padding(padding: EdgeInsets.only(right: 6), child:
+        Styles().images.getImage('location', excludeFromSemantics: true),
         ),
+        Expanded(child:
+        Text(_building?.fullAddress ?? '', style:
+        Styles().textStyles.getTextStyle("widget.button.light.title.medium.underline")
+        ),
+        )
+      ],),
       ),
-    );
+      ),
+      );
 
   Widget _buildShare() =>
-    InkWell(onTap: _onShare, child:
+      InkWell(onTap: _onShare, child:
       Padding(padding: EdgeInsets.symmetric(vertical: 10, ), child:
-        Row(children: [
-          Padding(padding: EdgeInsets.only(right: 6), child:
-            Styles().images.getImage('share', excludeFromSemantics: true),
-          ),
-          Expanded(child:
-            Text(Localization().getStringEx('panel.explore_building_detail.detail.share', 'Share This Location'), style:
-              Styles().textStyles.getTextStyle("widget.button.light.title.medium.underline")
-            ),
-          )
-        ],),
+      Row(children: [
+        Padding(padding: EdgeInsets.only(right: 6), child:
+        Styles().images.getImage('share', excludeFromSemantics: true),
+        ),
+        Expanded(child:
+        Text(Localization().getStringEx('panel.explore_building_detail.detail.share', 'Share This Location'), style:
+        Styles().textStyles.getTextStyle("widget.button.light.title.medium.underline")
+        ),
+        )
+      ],),
       ),
-    );
+      );
 
   // ignore: unused_element
   Widget _buildFloorPlansAndAmenities() =>
-    Visibility(visible: _canFloorPlansAndAmenities(), child:
+      Visibility(visible: _canFloorPlansAndAmenities(), child:
       InkWell(onTap: _onFloorPlansAndAmenities, child:
-        Padding(padding: EdgeInsets.symmetric(vertical: 10, ), child:
-          Row(children: [
-            Padding(padding: EdgeInsets.only(right: 6), child:
-              Styles().images.getImage('floorplan', excludeFromSemantics: true),
-            ),
-            Expanded(child:
-              Text(Localization().getStringEx('panel.explore_building_detail.detail.fllor_plan_and_amenities', 'Floor Plans & Amenities'), style:
-                Styles().textStyles.getTextStyle("widget.button.light.title.medium.underline")
-              ),
-            )
-          ],),
+      Padding(padding: EdgeInsets.symmetric(vertical: 10, ), child:
+      Row(children: [
+        Padding(padding: EdgeInsets.only(right: 6), child:
+        Styles().images.getImage('floorplan', excludeFromSemantics: true),
         ),
+        Expanded(child:
+        Text(Localization().getStringEx('panel.explore_building_detail.detail.fllor_plan_and_amenities', 'Floor Plans & Amenities'), style:
+        Styles().textStyles.getTextStyle("widget.button.light.title.medium.underline")
+        ),
+        )
+      ],),
       ),
-    );
-
-  Widget _buildSelectLocation() {
-    Widget? selectorWidget = widget.selectLocationBuilder?.call(context, ExploreSelectLocationContext.detail, explore: _building);
-    return (selectorWidget != null) ? Padding(padding: EdgeInsets.only(top: 32), child: selectorWidget) : Container();
-  }
-
+      ),
+      );
 
   Widget _buildLoadingContent() => Center(child:
-    Padding(padding: EdgeInsets.zero, child:
-      SizedBox(width: 32, height: 32, child: _loadingBuilding ?
-        CircularProgressIndicator(color: Styles().colors.fillColorSecondary, strokeWidth: 3, ) : null
-      )
-    ),
+  Padding(padding: EdgeInsets.zero, child:
+  SizedBox(width: 32, height: 32, child: _loadingBuilding ?
+  CircularProgressIndicator(color: Styles().colors.fillColorSecondary, strokeWidth: 3, ) : null
+  )
+  ),
   );
 
   Widget _buildErrorContent() => Center(child:
-    Padding(padding: const EdgeInsets.symmetric(vertical: 64, horizontal: 64), child:
-        Text(Localization().getStringEx('panel.explore_building_detail.message.failed', 'Failed to load location details'), style: Styles().textStyles.getTextStyle("widget.message.large"), textAlign: TextAlign.center,)
-    ),
+  Padding(padding: const EdgeInsets.symmetric(vertical: 64, horizontal: 64), child:
+  Text(Localization().getStringEx('panel.explore_building_detail.message.failed', 'Failed to load location details'), style: Styles().textStyles.getTextStyle("widget.message.large"), textAlign: TextAlign.center,)
+  ),
   );
 
   bool _canLocation() =>
-    StringUtils.isNotEmpty(_building?.fullAddress);
+      StringUtils.isNotEmpty(_building?.fullAddress);
 
   void _onLocation() {
     Analytics().logSelect(target: "Location Directions");
@@ -197,17 +188,18 @@ class _ExploreBuildingDetailPanelState extends State<ExploreBuildingDetailPanel>
     Analytics().logSelect(target: "Share This Location");
     if (_building != null) {
       Navigator.push(context, CupertinoPageRoute(builder: (context) =>
-        QrCodePanel.fromBuilding(_building, analyticsFeature: AnalyticsFeature.Map,)
+          QrCodePanel.fromBuilding(_building, analyticsFeature: AnalyticsFeature.Map,)
       ));
     }
   }
 
   bool _canFloorPlansAndAmenities() =>
-    true; // TODO: control when detail item should be viisble
+      true; // TODO: control when detail item should be viisble
 
   void _onFloorPlansAndAmenities() {
     Analytics().logSelect(target: "Floor Plans & Amenities");
     // TODO: present the relevant UI
+    open_webview();
   }
 }
 
@@ -241,21 +233,21 @@ class _WebViewAppState extends State<WebViewApp> {
     // Make the GET request
     http.Response? response = await Network().get(url, auth: Auth2());
 
-    // Process the response
-    if (response?.statusCode == 200) {
-      //Request successful, handle the response data
-      String? response_body = response?.body;
-      var json_response_body = jsonDecode(response_body!);
-
-      print('This Floorplan svg: ${json_response_body['svg']}');
-      return json_response_body;
+    int? responseCode = response?.statusCode;
+    String? responseString = response?.body;
+    if (responseCode == 200) {
+      try {
+        var json_response_body = jsonDecode(responseString!);
+        print(json_response_body);
+        print('This Floorplan svg: ${json_response_body['svg']}');
+        return json_response_body;
+      } on Exception catch (_) {
+        return '';
+      }
     } else {
-      // Request failed
-      print('Failed to get floorplan. Status code: ${response?.statusCode}');
+      print('Failed to load Floor plan. Response:\n$responseCode: $responseString');
+      return '';
     }
-
-    // Return the whole response
-    return '${response?.body}';
   }
 
 
